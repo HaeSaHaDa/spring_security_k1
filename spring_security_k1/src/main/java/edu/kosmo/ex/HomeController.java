2bd1d8cd638c69e92bf1097e92a57b5ec510a32c
DESKTOP-I0T918U\user <sixthgkrsus@naver.com>
2022-01-07 11:06:57 +0900

    first

---
+++ spring_security_k1/src/main/java/edu/kosmo/ex/HomeController.java
@@ -0,0 +1,54 @@
+package edu.kosmo.ex;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import lombok.extern.log4j.Log4j;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Log4j
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+
+	@GetMapping("/login/loginForm")//경로를 security-context의 설정과 맞춰줘
+	public String loginForm() {//void는 리퀘스트 매핑에 써 있는 /security 이 함수이 /admin이 붙어서 리턴
+		log.info("Welcome Login Form");
+		return "login/loginForm2";
+	}
+	@GetMapping("/user/userHome")//경로를 security-context의 설정과 맞춰줘
+	public String userHome() {//void는 리퀘스트 매핑에 써 있는 /security 이 함수이 /admin이 붙어서 리턴
+		log.info("Welcome Login Form");
+		return "user/userHome";
+	}
+}
